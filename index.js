3563108acc950daa229f23fed695f8f5f7038a5c
zaphod1984 <zaphod84@gmx.de>
2014-12-12 14:11:05 +0100

    dependency update
    * switch to splunkstorm v2.0.1
    * remove kvp-string building (done by splunkstorm)

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ var util = require("util");
 var winston = require("winston");
 var SplunkStorm = require("splunkstorm");
 
-var PATTERN_WHITESPACE_REPLACE = /[\r\n]/g;
-var PATTERN_WHITESPACE_DETECT = /\s/;
-var PATTERN_QUOTE = /"/g;
-
 var LEVEL_KEY = 'lvl';
 var MESSAGE_KEY = 'mssg';
 var HOST_KEY = 'hst';
@@ -24,15 +20,17 @@ var WinstonSplunkstorm = function (options) {
     var apiKey = options.apiKey;
     var projectId = options.projectId;
     var apiHostName = options.apiHostName;
+    var formatter = options.formatter || 'kvp';
 
-    if(!apiKey || !projectId || !apiHostName) {
+    if (!apiKey || !projectId || !apiHostName) {
         throw new Error('apiKey, projectId and apiHostName are neccessary');
     }
 
     this._storm = new SplunkStorm({
         apiKey: apiKey,
         projectId: projectId,
-        apiHostName: apiHostName
+        apiHostName: apiHostName,
+        formatter: formatter
     });
     this._sourceType = options.sourceType || 'syslog';
     this._source = options.source || '';
@@ -59,48 +57,28 @@ winston.transports.Splunkstorm = WinstonSplunkstorm;
  * @param callback function will be called when logging is done
  */
 WinstonSplunkstorm.prototype.log = function (level, message, meta, callback) {
-    var kvString = this._buildKeyValuePairs(level, message, meta);
-    var logMessage = util.format("%s %s", (new Date()).toISOString(), kvString);
-
-    this._storm.send(logMessage, this._sourceType, this._host, this._source, callback);
+    this._storm.send(this._buildParameters(level, message, meta), this._sourceType, this._host, this._source, callback);
 };
 
 /**
- * creates a string from key value pairs
- * @param level string implies the importance of the log message
- * @param message string the message that should be logged
- * @param pairs object a key value pair hash
- * @returns String
+ * build a hash representing the log message
+ * @param level
+ * @param message
+ * @param meta
+ * @returns {*|{}}
  * @private
  */
-WinstonSplunkstorm.prototype._buildKeyValuePairs = function(level, message, pairs) {
-    var parameters = pairs || {};
+WinstonSplunkstorm.prototype._buildParameters = function(level, message, meta) {
+    var parameters = meta || {};
 
-    if(message) {
+    if (message) {
         parameters[MESSAGE_KEY] = message;
     }
 
     parameters[LEVEL_KEY] = level;
     parameters[HOST_KEY] = this._options.host || os.hostname();
 
-    return Object.keys(parameters)
-        .map(function(key) {
-            var value = (parameters[key] + '')
-                .replace(PATTERN_WHITESPACE_REPLACE, '')
-                .replace(PATTERN_QUOTE, '\'')
-                .trim();
-
-            if(value.match(PATTERN_WHITESPACE_DETECT)) {
-                value = util.format('"%s"', value);
-            }
-
-            if(!value) {
-                value = '""';
-            }
-
-            return util.format('%s=%s', key, value);
-        })
-        .join(', ');
+    return parameters;
 };
 
 module.exports = WinstonSplunkstorm;
--- tests/tests.js
+++ tests/tests.js
@@ -5,36 +5,36 @@ var winston = require('winston');
 
 var WinstonSplunkstorm = require('../');
 
-describe('WinstonSplunkstorm', function() {
-    it('should register itself at the winston transports list', function() {
+describe('WinstonSplunkstorm', function () {
+    it('should register itself at the winston transports list', function () {
         expect(winston.transports.Splunkstorm).to.equal(WinstonSplunkstorm);
     });
 
-    it('should throw if no apiKey is set', function() {
-        expect(function() {
+    it('should throw if no apiKey is set', function () {
+        expect(function () {
             new WinstonSplunkstorm({projectId: 'foo', apiHostName: 'fooApiHost'});
         }).to.throw();
     });
 
-    it('should throw if no projectId is set', function() {
-        expect(function() {
+    it('should throw if no projectId is set', function () {
+        expect(function () {
             new WinstonSplunkstorm({apiKey: 'foo', apiHostName: 'fooApiHostName'});
         }).to.throw();
     });
 
-    it('should throw if no apiHostName is set', function() {
-        expect(function() {
+    it('should throw if no apiHostName is set', function () {
+        expect(function () {
             new WinstonSplunkstorm({apiKey: 'foo', projectId: 'fooProjectId'});
         }).to.throw();
     });
 
-    it('should throw if parameters are missing', function() {
-        expect(function() {
+    it('should throw if parameters are missing', function () {
+        expect(function () {
             new WinstonSplunkstorm({});
         }).to.throw();
     });
 
-    it('should create instance', function() {
+    it('should create instance', function () {
         var a = new WinstonSplunkstorm({
             projectId: 'projectId',
             apiKey: 'apiKey',
@@ -48,33 +48,7 @@ describe('WinstonSplunkstorm', function() {
         });
     });
 
-    describe('key value paris', function() {
-        it('should incorporate level and message into kvstring', function() {
-            var s = new WinstonSplunkstorm({apiKey: 'foo', projectId: 'foo1', apiHostName: 'foo2'});
-
-            var pairString = s._buildKeyValuePairs('info', 'foo');
-
-            expect(pairString).to.match(/mssg=foo, lvl=info, hst=.+/);
-        });
-
-        it('should incorporate level and message into kvstring', function() {
-            var s = new WinstonSplunkstorm({apiKey: 'foo', projectId: 'foo1', apiHostName: 'foo2'});
-
-            var pairString = s._buildKeyValuePairs('info', null, {
-                a: 'a',
-                b: 'b b',
-                c: 'c "foo" c',
-                d: 'd\nd',
-                e: ''
-            });
-
-            expect(pairString).to.match(/a=a, b="b b", c="c 'foo' c", d=dd, e="", lvl=info, hst=.+/);
-        });
-    });
-
-    it('should call log with the correct parameters', function() {
-        var clock = sinon.useFakeTimers();
-
+    it('should call log with the correct parameters', function () {
         var loggerInstance = new WinstonSplunkstorm({
             projectId: 'projectId',
             apiKey: 'apiKey',
@@ -89,9 +63,6 @@ describe('WinstonSplunkstorm', function() {
         };
 
         loggerInstance.log('info', 'foo', {a: 'a'}, 'callback');
-        expect(loggerInstance._storm.send).to.be.calledWith('1970-01-01T00:00:00.000Z a=a, mssg=foo, lvl=info, hst=fooHostName', 'fooSourceType', 'fooHostName', 'fooSource', 'callback');
-
-        clock.restore();
+        expect(loggerInstance._storm.send).to.be.calledWith({a: "a", hst: "fooHostName", lvl: "info", mssg: "foo" }, 'fooSourceType', 'fooHostName', 'fooSource', 'callback');
     });
-
 });
